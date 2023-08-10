6ac07547ad25ef9ed09c4cd96d381eb4d9436724
OksanaKorsun <oksana2081989@gmail.com>
2023-08-10 23:25:55 +0300

    Update task-10.js
    next-commit

--- js/task-10.js
+++ js/task-10.js
@@ -13,18 +13,26 @@ const elements = {
 elements.createBtn.addEventListener("click", createBoxes);
 elements.destroyBtn.addEventListener("click", destroyBoxes);
 
+let size = 20;
 function createBoxes(amount) {
-  amount = elements.quantity.valueAsNumber;
-  const step = 10;
+  // amount = elements.quantity.valueAsNumber;
+  amount = Number(elements.quantity.value);
+  const colors = [];
   for (let i = 1; i <= amount; i += 1) {
-    let box = document.createElement("div");
-    elements.boxes.appendChild(box);
-    box.style.width = `${30 + step * i}px`;
-    box.style.height = `${30 + step * i}px`;
+    const box = document.createElement("div");
+    // elements.boxes.appendChild(box);
+    size += 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
     box.style.backgroundColor = getRandomHexColor();
+    colors.push(box);
   }
+  elements.quantity.value = 0;
+  elements.boxes.append(...colors);
 }
 
 function destroyBoxes() {
   elements.boxes.innerHTML = "";
+  size = 20;
+  elements.quantity.value = 0;
 }
